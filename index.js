c812cdba845353f4401f2a33962cc6ec43f80cf3
unknown <hazim.sb@gmail.com>
2020-12-07 21:24:44 +0800

    q

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var open = require('open');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-res.send('hello world 7');
+res.send('Goodbye World');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
 open('http://18.138.250.62:3000/');
-console.log("index.js started\n browser http://18.138.250.62:3000/ will open");
+console.log("index.js started");
  
 module.exports = app;
