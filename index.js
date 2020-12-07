9dc3d21ee1b6dfab8304c148aa7d4123d6f2ca62
unknown <hazim.sb@gmail.com>
2020-12-07 21:29:12 +0800

    q

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var open = require('open');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-res.send('Goodbye World');
+res.send('hello World');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
