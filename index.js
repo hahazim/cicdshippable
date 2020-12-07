6546bcbec08859f920cec6abae4dc504ed5077a8
unknown <hazim.sb@gmail.com>
2020-12-07 21:06:24 +0800

    windows 6

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //importing node framework
 var express = require('express');
+var open = require('open');
  
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello world 5');
+res.send('hello world 6');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
+open('http://18.138.250.62:3000/');
 console.log("index.js started");
  
 module.exports = app;
