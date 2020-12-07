c2814feda83309632ef642491a069fc8e6f6cff7
unknown <hazim.sb@gmail.com>
2020-12-07 21:22:35 +0800

    q

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ res.send('hello world 7');
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
 open('http://18.138.250.62:3000/');
-console.log("index.js started");
+console.log("index.js started\n browser http://18.138.250.62:3000/ will open");
  
 module.exports = app;
